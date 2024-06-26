df2bc361d6d991ffb4af795d8ab5ef0e5ef0e0c4
behradhesami <138590435+behradhesami@users.noreply.github.com>
2024-06-26 14:54:57 +0330

    D

--- Object_Oriented/region.py
+++
@@ -1,8 +0,0 @@
-from Base import BaseClass
-
-class Region (BaseClass):#برای ایجاد محله ازش استفادخ میکنیم و تیاز به چیز خاصی نداره 
-    def __init__(self,name, *args, **kwargs) -> None:
-        
-        self.name = name
-        
-        super().__init__(*args, **kwargs)
